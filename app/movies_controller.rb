266a16b333afd664f98dc101de89909fe93f7a42
Erin Walker <erinb0317@hotmail.com>
2018-03-18 23:59:02 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,9 +22,11 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = {__})
+def can_be_created_in_a_block(args = {title: "The Room", release_date: 2003})
   Movie.create do |m|
-    __
+    m.title = args[:title]
+    m.release_date = args[:release_date]
+    m.save 
   end
 end
 
